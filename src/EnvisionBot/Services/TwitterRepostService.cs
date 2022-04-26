d273b07e1c7bf9be41ee86f9e848a9dcfb2461d2
Philipp Micke <archanyhm@gmail.com>
2022-04-26 02:44:16 +0200

    Enhance attachment logic
    Add some logging for attachment URLs to debug why GIFs are not being embedded properly.
    Also added support for videos. I guess.

--- src/EnvisionBot/Services/TwitterRepostService.cs
+++ src/EnvisionBot/Services/TwitterRepostService.cs
@@ -1,5 +1,5 @@
 using Microsoft.Extensions.Hosting;
-using Remora.Discord.API.Abstractions.Objects;
+using Microsoft.Extensions.Logging;
 using Remora.Discord.API.Abstractions.Rest;
 using Remora.Discord.API.Objects;
 using Remora.Rest.Core;
@@ -7,6 +7,7 @@ using TwitterSharp.Client;
 using TwitterSharp.Request;
 using TwitterSharp.Request.AdvancedSearch;
 using TwitterSharp.Request.Option;
+using TwitterSharp.Response.RMedia;
 using TwitterSharp.Rule;
 
 namespace EnvisionBot.Services;
@@ -17,10 +18,13 @@ public class TwitterRepostService : BackgroundService
 
     private readonly IDiscordRestChannelAPI _discordRestChannelApi;
 
-    public TwitterRepostService(TwitterClient twitterClient, IDiscordRestChannelAPI discordRestChannelApi)
+    private readonly ILogger<TwitterRepostService> _logger;
+
+    public TwitterRepostService(TwitterClient twitterClient, IDiscordRestChannelAPI discordRestChannelApi, ILogger<TwitterRepostService> logger)
     {
         _twitterClient = twitterClient;
         _discordRestChannelApi = discordRestChannelApi;
+        _logger = logger;
     }
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -30,24 +34,65 @@ public class TwitterRepostService : BackgroundService
         await _twitterClient.AddTweetStreamAsync(new StreamRequest(Expression.Author("archillect")));
         await _twitterClient.NextTweetStreamAsync((tweet) =>
         {
-            var embed = new Embed(
-                Description: $"{tweet.Text}",
-                Author: new EmbedAuthor(
-                    $"@{tweet.Author.Username}", 
-                    tweet.Author.Url, 
+            bool hasAttachment = tweet.Attachments.Media == null || tweet.Attachments.Media.Length < 1;
+
+            if (hasAttachment)
+            {
+                var firstAttachment = tweet.Attachments.Media!.First();
+
+                var embedDescription = tweet.Text;
+                var embedAuthor = new EmbedAuthor(
+                    $"@{tweet.Author.Username}",
+                    tweet.Author.Url,
                     tweet.Author.ProfileImageUrl
-                ),
-                Footer: new EmbedFooter(tweet.Source),
-                Image: tweet.Attachments.Media == null || tweet.Attachments.Media.Length == 0
-                    ? new Optional<IEmbedImage>()
-                    : new EmbedImage(tweet.Attachments.Media.First().Url)
-            );
-            Console.WriteLine(tweet.Author.Url);
-            _discordRestChannelApi.CreateMessageAsync(new Snowflake(965996569226772500), embeds: new[] {embed}, ct: stoppingToken);
+                );
+                var embedFooter = new EmbedFooter(tweet.Source);
+
+                if (firstAttachment.Type == MediaType.Photo)
+                {
+                    var embed = new Embed(
+                        Description: embedDescription,
+                        Author: embedAuthor,
+                        Footer: embedFooter,
+                        Image: new EmbedImage(firstAttachment.Url)
+                    );
+                    
+                    _discordRestChannelApi.CreateMessageAsync(new Snowflake(965996569226772500), embeds: new[] {embed}, ct: stoppingToken);
+                }
+                else if (firstAttachment.Type == MediaType.Video)
+                {
+                    var embed = new Embed(
+                        Description: embedDescription,
+                        Author: embedAuthor,
+                        Footer: embedFooter,
+                        Video: new EmbedVideo(firstAttachment.Url)
+                    );
+                    
+                    _discordRestChannelApi.CreateMessageAsync(new Snowflake(965996569226772500), embeds: new[] {embed}, ct: stoppingToken);
+                }
+                else if (firstAttachment.Type == MediaType.AnimatedGif)
+                {
+                    var embed = new Embed(
+                        Description: embedDescription,
+                        Author: embedAuthor,
+                        Footer: embedFooter,
+                        Image: new EmbedImage(firstAttachment.Url)
+                    );
+                    
+                    _discordRestChannelApi.CreateMessageAsync(new Snowflake(965996569226772500), embeds: new[] {embed}, ct: stoppingToken);
+                }
+                
+                _logger.LogInformation(
+                    $"Attachment URL: {firstAttachment.Url}"
+                );
+                
+                _logger.LogInformation(
+                    $"Attachment Type: {firstAttachment.Type.ToString()}"
+                );
+            }
         },
         new TweetSearchOptions
         {
-            
             UserOptions = new [] {UserOption.Profile_Image_Url, UserOption.Url},
             MediaOptions = new [] {MediaOption.Url, MediaOption.Preview_Image_Url},
             TweetOptions = new [] {TweetOption.Attachments, TweetOption.Source}
